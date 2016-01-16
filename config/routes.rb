9687d76c637ed64570a9ba01b6a787659c864b2f
Charlie McElfresh <charlie.mcelfresh@socialchorus.com>
2016-01-16 14:47:47 -0800

    Add MainController has placeholder for root page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get '/ping' => 'main#ping'
   resources :haikus, constraints: {format: :json} do
     resources :lines, only: [:index, :create, :update, :show, :destroy], constraints: {format: :json}
+  root :to => "main#index"
   end
   resources :users, only: [:create, :destroy]
   resources :api_keys, only: :create
+  resources :main, only: [:index]
 end
