2b0964c19b5d32298403cbe477172109613a8828
Charlie McElfresh <charlie.mcelfresh@socialchorus.com>
2016-01-16 12:27:19 -0800

    Remove unused code

--- app/serializers/line_serializer.rb
+++
@@ -1,3 +0,0 @@
-class LineSerializer < ActiveModel::Serializer
-  attributes :id, :haiku_id, :content
-end
--- app/serializers/user_serializer.rb
+++
@@ -1,3 +0,0 @@
-class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :password_hash, :password_salt
-end
