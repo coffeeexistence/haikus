ccc1e4f54f1edcc6c48ab510a07410dfd2e42115
Charlie McElfresh <charlie.mcelfresh@socialchorus.com>
2016-01-16 18:03:56 -0800

    Add users_spec.rb

---
+++ spec/request/users_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "user", type: :request do
+  let!(:user) { FactoryGirl.build(:user) }
+  let(:params) {{ user: { email: user.email, password: user.password, password_confirmation: user.password} } }
+
+  it "should render the html" do
+    get '/sign_up'
+    expect(response.code).to eq("200")
+  end
+
+  it "should create a user" do
+    post '/users', params
+    expect(response.code).to eq("302")
+    response.should redirect_to(root_path)
+    e = params[:user][:email]
+    expect(User.where(email: e).first.email).to eq(e)
+  end
+end
