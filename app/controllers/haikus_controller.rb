dcaa99a2f896be246c52bb3118caa8cc5ae11aa8
kangkyu <kangkyu1111@gmail.com>
2016-01-17 00:42:14 -0800

    Add haikus#create template and action
    should create a haiku with its first line
    Add create and new method, controller test for HaikusController
    remove OmniAuth rails_helper config (error on test run, we do not use omniauth)

--- app/controllers/haikus_controller.rb
+++ app/controllers/haikus_controller.rb
@@ -1,46 +1,24 @@
 class HaikusController < ApplicationController
-  def index
-    haikus = Haiku.all
-    render json: haikus
-  end
 
-  def show
-    haiku = Haiku.find(params[:id])
-    render json: haiku
+  def new
+    @haiku = Haiku.new
+    @haiku.lines.build
   end
 
   def create
-    haiku = Haiku.new(haiku_params)
-    if haiku.save
-      render json: haiku.lines, status: 201
+    @haiku = Haiku.new(haiku_params)
+    if @haiku.save
+      redirect_to root_url, notice: "Haiku created!"
     else
-      render json: '400', status: 400
+      render "new"
     end
   end
 
-  def update
-    haiku = Haiku.find(params[:id])
-    if haiku.lines.first.update_attributes(line_params)
-      render json: haiku.lines
-    else
-      render json: '400', status: 400
-    end
-  end
-
-  def destroy
-    haiku = Haiku.find(params[:id])
-    haiku.destroy
-    head 204
-  end
 
   private
 
   def haiku_params
-    params.permit(lines_attributes: [:haiku_id, :content])
+    params.require(:haiku).permit(lines_attributes: [:content])
   end
 
-  def line_params
-    params.permit(:haiku_id, :content)
-  end
-  
 end
