6dc323883b5ee08dc60f90dd627af2d88fb8e3f5
kangkyu <kangkyu1111@gmail.com>
2016-01-21 23:36:09 -0800

    Test haikus/index route and template

--- spec/requests/haikus_spec.rb
+++ spec/requests/haikus_spec.rb
@@ -5,6 +5,14 @@ describe "haikus", type: :request do
   let!(:user) { FactoryGirl.create(:user) }
   let(:params) {{ email: user.email, password: user.password } }
 
+  describe 'reading haikus' do
+    it "should render haikus index template" do
+      get '/haikus'
+      expect(response).to have_http_status(200)
+      expect(response).to render_template('index')
+    end
+  end
+
   describe 'writing haiku' do
     it "should render the html" do
       get '/haikus/new'
