d9fa9b7052bdf3b229230348ee3d3291f641abd1
Mike <mikeknueppel@hotmail.com>
2016-01-23 11:02:21 -0800

    remove trailing spaces

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    sequence(:email) { |n| "user#{n}@factory.com" }    
+    sequence(:email) { |n| "user#{n}@factory.com" }
     password "password"
   end
 end
