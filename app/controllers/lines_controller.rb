00152f3215d33c4ea8d3b43aaac2a3dcab77320a
kangkyu <kangkyu1111@gmail.com>
2016-01-23 20:11:28 -0800

    remove old lines_controller actions from haikus-api (for json response) and json request helper module
    lines#index, update, show, destroy
    spec/support/request_helpers.rb

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -1,11 +1,6 @@
 class LinesController < ApplicationController
   before_action :require_login, only: [:create]
 
-  def index
-    lines = Line.eager_load(:haiku).where("haiku_id = ?", params[:haiku_id])
-    render json: lines, each_serializer: LineSerializer
-  end
-
   def new
     @haiku = Haiku.find(params[:haiku_id])
     @count = @haiku.lines.count
@@ -22,26 +17,6 @@ class LinesController < ApplicationController
       @count = @haiku.lines.count
       render 'new'
     end
-  end
-
-  def update
-    line = Line.find(params[:id])
-    if line.update_attributes(line_params)
-      render json: line
-    else
-      render json: '400', status: 400
-    end
-  end
-
-  def show
-    line = Line.find(params[:id])
-    render json: line
-  end
-
-  def destroy
-    line = Line.find(params[:id])
-    line.destroy
-    head 204
   end
 
   private
