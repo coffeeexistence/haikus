99cd84e0131ea9ebdc81c84cf4a1d53e42d47e2a
jendiamond <thejendiamond@gmail.com>
2016-02-03 17:39:43 -0800

    Add Word Model
    [feature/112400429-jd](https://www.pivotaltracker.com/projects/1516637/stories/112400429)
    
    Add list of words from:
    http://taralazar.com/2014/06/09/list-of-200-fun-cool-and-interesting-words/

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160124142217) do
+ActiveRecord::Schema.define(version: 20160201230541) do
 
   create_table "friendships", force: :cascade do |t|
     t.integer  "user_id"
@@ -43,6 +43,12 @@ ActiveRecord::Schema.define(version: 20160124142217) do
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
     t.string   "forgot_password_uuid"
+  end
+
+  create_table "words", force: :cascade do |t|
+    t.string   "word"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
---
+++ spec/factories/words.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :word do
+    word "MyString"
+  end
+end
