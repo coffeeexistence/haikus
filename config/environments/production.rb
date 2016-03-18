5c73210ef3735eb57ca52f0dd377424be98195e5
Thomas Jung <thomasjunginbox@gmail.com>
2016-03-17 21:00:08 -0700

    Change mailer to environment variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,8 +71,8 @@ Rails.application.configure do
     address:                  'smtp.gmail.com',
     port:                      587,
     domain:                   'gmail.com',
-    user_name:                'haikus.helpdesk@gmail.com',
-    password:                  ENV['GMAIL'],
+    user_name:                 ENV['GMAIL_USERNAME'],
+    password:                  ENV['GMAIL_PASSWORD'],
     authentication:           'plain',
     enable_starttls_auto:      true,
   }
